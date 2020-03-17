023d96524e31707336133ca2190cd1235e1263cf
Lee Leibrandt <leeleibrandt9@gmail.com>
2020-03-17 10:06:13 +0200

    Compare functionality / Layout/ Design

--- class.php
+++ class.php
@@ -1,114 +1,77 @@
-<div class="container">
-    <div class="grid-container">
-        <div class="grid-item">
-            <?php
+<div class="grid-item">
+    <?php
+        //user input details on submit
+        if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['email']))
+        {
+            //declare variables
+            $name = $_POST['name'];
+            $surname = $_POST['surname'];
+            $email = $_POST['email'];
+ 
+            function userInfo($n, $s, $e )
+            {
+            return "<br />" . "$n". "$s" ."$e";
+            }
 
-                //user input details on submit
-                if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['email']))
-                {
-                  //declare variables
-                  $name = $_POST['name'];
-                  $surname = $_POST['surname'];
-                  $email = $_POST['email'];
-                }
-
-                function userInfo($n, $s, $e )
-                {
-                  return "<br />" . "$n". "$s" ."$e";
-                }
-
-                //outputting details of user           
-                echo "<div class = 'banner'>";
-                    echo "<center><font size='5px'>User Details</font></center>";
-                    echo "Name:" ." ". "<b>$name</b>";
-                    echo "<br />";
-                    echo "Surname:" ." ". "<b>$surname</b>";
-                    echo "<br />";
-                    echo "Email:" ." ". "<b>$email</b>";
-                echo "</div>";    
-            ?>
-        </div>
-
-        <div class="grid-item">
-            <?php
-                class Hotel1
-                {
-                    const BR = '<br />';
-
-                    var $price;
-                    var $name;
-                    var $photo; 
-                    var $dir = 'img/';
-
-                    public function print_hotel()
-                    {              
-                        echo "<div class = banner>";
-                            echo "<center><font size='5px'> {$this->name} </font></center> ".self::BR;
-                            echo "<center><img src = '{$this->dir}{$this->photo}'></center>".self::BR;
-                            echo "<center><font size = '5px'>Daily Rate: R".$this->price . "</font size></center>" .self::BR;
-                            echo "Pool: ".$this->pool.self::BR;
-                            echo "bar: ".$this->bar.self::BR;
-                            echo "spa: ".$this->spa.self::BR;
-                            echo "kidfriendly: ".$this->kidfriendly.self::BR;
-                        echo "</div>";           
-                    }
-                }
-        
-                $hotel = new Hotel1;
-
-                $hotel -> name = $_POST['hotel1'];
-                $hotel -> photo =  'sun.jpeg';
-                $hotel -> price = 500;
-                $hotel -> pool = 'true';
-                $hotel -> bar = 'true';
-                $hotel -> spa = 'true';
-                $hotel -> kidfriendly = 'true';
-
-                $hotel -> print_hotel();
-
-            ?>
-
-        </div>
-
-        <div class="grid-item">
-            <?php
-                class Hotel2 
-                {
-                    const BR = '<br />';
-
-                    var $price;
-                    var $name;
-                    var $photo; 
-                    var $dir = 'img/';
-
-                    public function print_hotel()
-                    {                      
-                        echo "<div class = banner>";
-                            echo "<center><font size='5px'> {$this->name} </font></center> ".self::BR;
-                            echo "<center><img src = '{$this->dir}{$this->photo}'></center>".self::BR;
-                            echo "<center><font size = '5px'>Daily Rate: R".$this->price . "</font size></center>".self::BR;
-                            echo "Pool: ".$this->pool.self::BR;
-                            echo "bar: ".$this->bar.self::BR;
-                            echo "spa: ".$this->spa.self::BR;
-                            echo "kidfriendly: ".$this->kidfriendly.self::BR;
-                        echo "</div>";          
-                    }
-                }
-
-                $hotel = new Hotel2;
-
-                $hotel -> name = $_POST['hotel2'];
-                $hotel -> photo = 'break.jpg'; 
-                $hotel -> price = 250;
-                $hotel -> pool = 'true';
-                $hotel -> bar = 'false';
-                $hotel -> spa = 'false';
-                $hotel -> kidfriendly = 'true';
-
-                $hotel -> print_hotel();
-            ?>
-        </div>
-        
-       
-    </div>
+            //outputting details of user                
+            echo "<center><font size='5px'>User Details</font></center>";
+            echo "<br />";
+            echo "<font size = '4px'>Name:" ." ". "$name</font size>";
+            echo "<br />";
+            echo "<font size = '4px'>Surname:" ." ". "$surname</font size>";
+            echo "<br />";
+            echo "<font size = '4px'>Email:" ." ". "$email</font size>";
+        }         
+    ?>
 </div>
+
+<?php
+    class Hotel1
+    {
+        const BR = '<br />';
+
+        var $price;
+        var $name;
+        var $photo; 
+        var $dir = 'img/';
+
+        public function print_hotel()
+        {                       
+            echo "<center><font size='5px'> {$this->name} </font></center> ".self::BR;
+            echo "<center><img src = '{$this->dir}{$this->photo}'></center>".self::BR;
+            echo "<center><font size = '5px'>Daily Rate: R".$this->price . "</font size></center>".self::BR;
+            echo "<center><font size = '4px'> Check In: ".$this->checkin."</font size></center>";
+            echo "<center><font size = '4px'> Check Out: ".$this->checkout."</font size></center>";
+            echo "Pool: ".$this->pool.self::BR;
+            echo "bar: ".$this->bar.self::BR;
+            echo "spa: ".$this->spa.self::BR;
+            echo "kidfriendly: ".$this->kidfriendly.self::BR;          
+        }
+    }
+?>
+
+<?php
+    class Hotel2 
+    {
+        const BR = '<br />';
+
+        var $price;
+        var $name;
+        var $photo; 
+        var $dir = 'img/';
+
+        public function print_hotel()
+        {                               
+            echo "<center><font size='5px'> {$this->name} </font></center> ".self::BR;
+            echo "<center><img src = '{$this->dir}{$this->photo}'></center>".self::BR;
+            echo "<center><font size = '5px'>Daily Rate: R".$this->price . "</font size></center>".self::BR;
+            echo "<center><font size = '4px'> Check In: ".$this->checkin."</font size></center>";
+            echo "<center><font size = '4px'> Check Out: ".$this->checkout."</font size></center>";
+            echo "Pool: ".$this->pool.self::BR;
+            echo "bar: ".$this->bar.self::BR;
+            echo "spa: ".$this->spa.self::BR;
+            echo "kidfriendly: ".$this->kidfriendly.self::BR;                   
+        }
+    }
+?>
+    
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
     <link rel="stylesheet" href="css/style.css" text="text/css">
-    <title>Hotel Travago</title>
+    <title>Hotel Trivago</title>
   </head>
   <body>
 
@@ -16,15 +16,14 @@
       <a class="navbar-brand" href="#">
         <img src="img/AVENGERS LOGO VERSION 2.png" width="200px"alt="avenger">
       </a>
-      <center><h1>LERACA Hotel</h1>
- 
+      <center><h1>LERACA Hotel</h1>  
     </nav>
 
-    <div class="container">
+   <center><div class="grid-item">
       <!--user inputs-->
       <form action="index.php" method="post" class="form">
           Please provide following details below:<br>
-          <input type="text" name="name" placeholder="Name" required><br>
+          <input type="text" name="name" placeholder="Nam4e" required><br>
           <input type="text" name="surname" placeholder="Surname" required><br>
           <input type="email" name="email" placeholder="Email" required><br>
 
@@ -34,7 +33,7 @@
           1 <select name="hotel1" id="hotel" required>
             <option>---Select Hotel Here---</option>
             <?php
-              $hotelname = array("The New Tulbagh Hotel", "DoubleTree by Hilton Hotel", "Sea Point Inn", "Mojo Hotel");
+              $hotelname = array("Sea Point Inn", "Mojo Hotel");
               foreach ($hotelname as $hotel) {
             ?>
             <center><option> <?php echo $hotel; ?> </option>
@@ -44,7 +43,7 @@
           2 <select name="hotel2" id="hotel" required>
             <option>---Select Hotel Here---</option>
             <?php 
-              $hotelname = array("The New Tulbagh Hotel", "DoubleTree by Hilton Hotel", "Sea Point Inn", "Mojo Hotel");
+              $hotelname = array("Sea Point Inn", "Mojo Hotel");
               foreach ($hotelname as $hotel) {
             ?>
             <option> <?php echo $hotel; ?> </option>
@@ -54,26 +53,95 @@
           Check-In <input type="date" name="calenderIn" required> 
           Check-Out <input type="date" name="calenderOut" required><br>
         <input type="submit">
+      </form>
     </div>
-    </div><br>
-
+    <br>  
+    <center>
     <?php
+    
       //LINKING EXTERNAL PHP PAGE
       include 'class.php';
-   
+      
+      //Logical arguments
+      //Logical outputs for hotel one
+      if($_POST)
+      {
+      echo "<div class='grid-item'>";
+        if ($_POST['hotel1'] == $hotelname[0])
+        {
+          $hotel = new Hotel1;
+          
+          $hotel -> name = $_POST['hotel1'];
+          $hotel -> photo =  'sun.jpeg';
+          $hotel -> price = 500;
+          $hotel -> checkin = $_POST['calenderIn'];
+          $hotel -> checkout = $_POST['calenderOut'];
+          $hotel -> pool = 'true';
+          $hotel -> bar = 'true';
+          $hotel -> spa = 'true';
+          $hotel -> kidfriendly = 'true';
+
+          $hotel -> print_hotel();
+        }
+        elseif ($_POST['hotel1'] == $hotelname[1])
+        {
+          $hotel = new Hotel1;
+
+          $hotel -> name = $_POST['hotel2'];
+          $hotel -> photo = 'break.jpg'; 
+          $hotel -> price = 350;
+          $hotel -> checkin = $_POST['calenderIn'];
+          $hotel -> checkout = $_POST['calenderOut'];
+          $hotel -> pool = 'true';
+          $hotel -> bar = 'false';
+          $hotel -> spa = 'false';
+          $hotel -> kidfriendly = 'true';
+
+          $hotel -> print_hotel();
+        }
+        echo "</div>";  
+
+        //Logical outputs for hotel two
+        echo "<div class='grid-item'>";
+          if ($_POST['hotel2'] == $hotelname[0])
+          {
+            $hotel = new Hotel2;
+            
+            $hotel -> name = $_POST['hotel2'];
+            $hotel -> photo =  'sun.jpeg';
+            $hotel -> price = 500;
+            $hotel -> checkin = $_POST['calenderIn'];
+            $hotel -> checkout = $_POST['calenderOut'];
+            $hotel -> pool = 'true';
+            $hotel -> bar = 'true';
+            $hotel -> spa = 'true';
+            $hotel -> kidfriendly = 'true';
+
+            $hotel -> print_hotel();
+          }
+          elseif ($_POST['hotel2'] == $hotelname[1])
+          {
+            $hotel = new Hotel2;
+
+            $hotel -> name = $_POST['hotel2'];
+            $hotel -> photo = 'break.jpg'; 
+            $hotel -> price = 350;
+            $hotel -> checkin = $_POST['calenderIn'];
+            $hotel -> checkout = $_POST['calenderOut'];
+            $hotel -> pool = 'true';
+            $hotel -> bar = 'false';
+            $hotel -> spa = 'false';
+            $hotel -> kidfriendly = 'true';
+
+            $hotel -> print_hotel();
+          }
+        echo "</div>"; 
+      } 
     ?>
-
-        
-
-
-
-
-
-
-
+    </center>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
-</html>
+</html>   
