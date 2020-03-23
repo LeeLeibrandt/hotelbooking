507c5f99db8c047ea83c37fdca4ff0be8c55c242
Admin <Admin@MrRbot.local>
2020-03-23 21:34:54 +0200

    spacing

--- functionality.php
+++ functionality.php
@@ -221,7 +221,6 @@
       echo"<hr>";
 
     ?>
-
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
   </body>
